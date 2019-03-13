3a1810f38745af14bb7313f0a897625bc8b15007
AAsriyan <aasriyan@protonmail.com>
2019-03-13 16:55:54 -0400

    Fixed adding the categories

--- lifegpa/src/components/onboarding/CategoryList.js
+++ lifegpa/src/components/onboarding/CategoryList.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import "./Categories.css";
 
 export class CategoryList extends Component {
   render() {
     return (
-      <div>
-        <p>Category List</p>
+      <div
+        className="category-added"
+        style={{ border: `2px solid ${this.props.category.color}` }}
+      >
+        <p
+          style={{
+            color: this.props.category.color
+          }}
+        >
+          {this.props.category.categoryTitle}
+        </p>
       </div>
     );
   }
